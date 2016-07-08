9f8785ebaf40cce96963df5cf4934071a9daba2e
Lars Stegman <l.s.stegman@student.tudelft.nl>
2016-07-08 16:49:34 +0200

    Initial Commit

---
+++ OvTijd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OvTijd
+//
+//  Created by Lars Stegman on 08-07-16.
+//  Copyright © 2016 Stegman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
